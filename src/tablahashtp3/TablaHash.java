5faf7c605747a0c839dde81ea11dc9c099852746
Martin-J-Larre <martinjlarre@gmail.com>
2024-04-21 23:48:39 -0400

    #7 terminado

--- src/tablahashtp3/TablaHash.java
+++ src/tablahashtp3/TablaHash.java
@@ -17,10 +17,17 @@ public class TablaHash {
         
     }
     
-    // FuncionHash asigna una clave
+    // La funcionHash retorna una clave al resolver el modulo 
+    // de una entrada de un valor y el tamaño de una tabla,
+    // donde n es el valor y m es el tamaño de la tabla (tendiendo 
+    // a ser un numero primo), primero debemos asegurarnos que la clave
+    // no sea un numero negativo para que pueda entrar en el rango 
+    // positivo de la tabla, para esto utilizamos el operador BitWise & con 
+    // n y el exadecimal 0x7fffffff (max numeros enteros en Java) esta 
+    // operacion va hacer que n mantenga el valor pero lo va a forzar a 
+    // ser siempre positivo para despues calcular el modulo con m.
     static int funcionHash(int n, int m){
         return (n & 0x7fffffff) % m;    
-
     }
     
     static void insertaHash(TablaHash[] h, int m, int n) {
@@ -48,17 +55,17 @@ public class TablaHash {
     
     
         // 1 - Hashear n y guardar el resultado de la llave en j
-        // 2 - En un loop ejecutar la posicion de la llave j y en esa iteracion checkear
-        // .   las condiciones, en el caso de no econtar el dato buscado
-        // .   incrementar j+= y ejecutar el proceso en la suiente iteracion, ya 
-        //     que el dato que se busca puede estar en otra posicion por colision.   
-        // 3 - checkear si estado es 0 (vacio) y retornar -1
-        // 4 - chechear si hay coincide el dato y si estado es 1 (eliminado) entonces 
+        // 2 - En un loop ejecutar la posicion de la llave j y sino pasa la 
+        //     condicion del loop retornar -1 y pasa en esa iteracion checkear
+        //     las condiciones.
+        // 3 - checkear si el estado de j es 0 (vacio) y retornar -1 sino seguir
+        // 4 - chechear si coincide el dato y si estado es 1 (eliminado) entonces 
         // .   retornar -1 sino retornar el dato buscado.
         // 5 - Si ninguna de los condiciones es vedadera incrementar j+= y ejecutar 
-        //     el proceso en la suiente iteracion hasta que no pase la condicion del loop y
-        //     retorne -1 sin poder encontar el dato.
-    
+        //     el proceso en la suiente iteracion ya que el dato que se busca
+        //     puede estar en la sigueinte posicion por colision y sino se encuentra 
+        //     el dato salir del loop y retornar -1
+
     static int buscaHash(TablaHash[] h, int m, int n) {
 
         int j = funcionHash(n, m);
